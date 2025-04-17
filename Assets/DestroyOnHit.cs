001b1cdec79a4431dc22697c457c7b2a4a483f8e
Yanis Abbar <yanis.abbar93@gmail.com>
2025-04-17 14:03:33 +0200

    Son ajouté

--- Assets/DestroyOnHit.cs
+++ Assets/DestroyOnHit.cs
@@ -2,10 +2,29 @@ using UnityEngine;
 
 public class DestroyOnHit : MonoBehaviour
 {
+    private AudioClip destructionSound;
+    public float volume = 1.0f;
+
+    private void Start()
+    {
+        // Charge le son depuis le dossier Resources/Sound
+        destructionSound = Resources.Load<AudioClip>("Sound/asteroid-explosion");
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Missile"))
         {
+            // Joue le son si il a été trouvé
+            if (destructionSound != null)
+            {
+                AudioSource.PlayClipAtPoint(destructionSound, transform.position, volume);
+            }
+            else
+            {
+                Debug.LogWarning("Le son d'explosion n'a pas été trouvé !");
+            }
+
             Destroy(collision.gameObject);
             Destroy(gameObject);
         }
